39ca92d184a4cefc6b25311efc21d13cd52d5b78
anupriyalathey <anupriyalathey@gmail.com>
2024-01-25 10:56:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      choices: [],
+      selectedChoices: {},
+    };
+  }
+
+  componentDidMount() {
+    // Fetch feedback questions and choices from the API
+    fetch('https://brijfeedback.pythonanywhere.com/api/get-feedback-questions/?unitID=1')
+      .then(response => response.json())
+      .then(data => {
+        const questions = data.feedbackQuestions;
+        const choices = data.choices;
+        const selectedChoices = {};
+        questions.forEach((question, index) => {
+          selectedChoices[question] = choices[index][0]; // Default selection
+        });
+        this.setState({ questions, choices, selectedChoices });
+      })
+      .catch(error => console.error('Error fetching feedback questions:', error));
+  }
+
+  handleChoiceChange = (question, choice) => {
+    const { selectedChoices } = this.state;
+    selectedChoices[question] = choice;
+    this.setState({ selectedChoices });
+  };
+
+  handleSubmit = () => {
+    const { questions, selectedChoices } = this.state;
+    const feedback = {
+      questions,
+      choices: questions.map(question => selectedChoices[question]),
+    };
+    console.log('Submitted Feedback:', feedback);
+    // You can send the feedback data to the backend here if needed
+  };
+
+  render() {
+    const { questions, choices, selectedChoices } = this.state;
+
+    return (
+      <div>
+        <form>
+          {questions.map((question, index) => (
+            <div key={index}>
+              <p>{question}</p>
+              {choices[index].map((choice, choiceIndex) => (
+                <label key={choiceIndex}>
+                  <input
+                    type="radio"
+                    name={`choice-${index}`}
+                    value={choice}
+                    checked={selectedChoices[question] === choice}
+                    onChange={() => this.handleChoiceChange(question, choice)}
+                  />
+                  {choice}
+                </label>
+              ))}
+            </div>
+          ))}
+          <button type="button" onClick={this.handleSubmit}>
+            Submit Feedback
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
