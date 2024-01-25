732ce7021cbd35aa6ef1fb4032ac5a8aa69305a1
anupriyalathey <anupriyalathey@gmail.com>
2024-01-26 03:57:09 +0530

    changed font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import theme from './theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
